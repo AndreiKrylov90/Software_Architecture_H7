a06e53fb9a3c9a9fd4c70051f6cb5b6777c66006
Andrei.Krylov <dronovoy@yandex.ru>
2023-12-13 12:15:49 +0300

    it starts working

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
